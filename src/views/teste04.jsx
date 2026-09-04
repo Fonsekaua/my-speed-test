ec503572d123c3ffed22c1c9d5c7ff8c9de135eb
fonsekaua <fonsecakauasilva3@gmail.com>
2026-09-04 16:05:51 -0500

    finalizado teste 04 - via cep

--- src/views/teste04.jsx
+++ src/views/teste04.jsx
@@ -2,15 +2,72 @@
 import { useState } from "react"
 
 const BuscarCEP = () => {
-    const [input,setInput] = useState("")
+    const [input, setInput] = useState("")
+    const [dados, setDados] = useState({})
+    const [erro, setErro] = useState("")
+    const [consulta,setConsulta] = useState('')
+    const MeuCep = async () => {
+       
+        if (input.length != 8) {
+            setErro("Cep deve ter 8 caracteres")
+        }
+        else if (isNaN(Number(input))) {
+            setErro("CEP so pode possuir numeros")
+        }
+        else {
+            try {
+                setErro("")
+                setDados({})
+                const resposta = await fetch(`https://viacep.com.br/ws/${input}/json/`)
+                const data = await resposta.json()
+                setConsulta(true)
+                setConsulta("Consultando...")
+                setTimeout(() => {
+                    if(data.cep) {
+                    setConsulta("Encontrado!!")
+                    setDados(data)
+                    }else {
+                    setConsulta("Não Encontrado!!")
+
+                    }
+                }, 1000);
+
+                setInput("")
+            } catch (err) {
+                 
+                setErro("Não foi possivel encontrar seu cep")
+            }
+        }
+
+    }
     return (
         <div className="w-screen h-screen bg-zinc-900 text-white">
-           <div>
-             <input type="text" placeholder="Digite seu cep" />
-            <button>
-                Buscar
-            </button>
-           </div>
+            <div>
+                
+                <input type="text" placeholder="Digite seu cep" value={input} onChange={(e) => setInput(e.target.value)} />
+                <button onClick={MeuCep}>
+                    Buscar
+                </button>
+            </div>
+            {
+                consulta && <p>{consulta}</p>
+            }
+            {
+                erro ? (
+                    <p>Erro: {erro}</p>
+                ) : (
+                    <ul>
+                        <li>{dados.cep}</li>
+                        <li>{dados.complemento}</li>
+                        <li>{dados.bairro}</li>
+                        <li>{dados.estado}</li>
+                        <li>{dados.uf}</li>
+                        <li>{dados.ddd}</li>
+
+                    </ul>
+                    )
+                
+            }
         </div>
     )
 }
