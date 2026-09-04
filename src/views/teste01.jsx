1471efedc6f730c0cf520c52fb65a7d708ca0499
fonsekaua <fonsecakauasilva3@gmail.com>
2026-09-04 14:45:16 -0500

    finalizado troca de estilos - teste01

--- src/views/teste01.jsx
+++ src/views/teste01.jsx
@@ -3,9 +3,9 @@ import { useState } from "react"
 
 const TrocarTema = () => {
     const [tema,definirTema] = useState(false)
-    
+
     return (
-        <div className={`w-screen h-screen`} >
+        <div className={`w-screen h-screen ${tema?"claro":"escuro"}`} >
             <button className="bg-mist-500 p-1" onClick={() => definirTema(prev => !prev)}>
                 Tema atual - {tema?"claro":'escuro'}
             </button>
