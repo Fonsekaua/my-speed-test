b8ad4fed228549670d582937fcecf2c7d72f28f6
fonsekaua <fonsecakauasilva3@gmail.com>
2026-09-04 15:24:01 -0500

    criado sistema do historico com identificação

--- src/views/teste03.jsx
+++ src/views/teste03.jsx
@@ -3,32 +3,54 @@ import { useState } from "react"
 
 const ContadorHistorico = () => {
     const [contador,setContador] = useState(0);
+    const [historico,setHistorico] = useState([])
     const incrementar = () => {
         setContador(contador + 1)
+        setHistorico(prev => [...prev,{status:"incrementado",valor:contador + 1}])
     }
     const decrementar = () => {
-        if(contador>=0){
+        if(contador>0){
             setContador(contador - 1)
+             setHistorico(prev => [...prev,{status:"decrementado",valor:contador - 1}])
         }
     }
 
     const zerar = () => {
         setContador(0)
+         setHistorico(prev => [...prev,{status:"zerado",valor:0}])
     }
     return (
-        <div className={`w-screen h-screen bg-zinc-900 text-white flex items-center justify-center flex-col`}>
+        <div className={`w-screen h-screen bg-zinc-900 text-white flex items-center justify-center flex-col relative`}>
+            <div className="absolute top-10 right-10 p-2">
+                <p>Historico</p>
+                {
+                    historico.length>0?(
+                        <ul className="overflow-y-scroll h-96 scrollbar-none flex flex-col-reverse items-start justify-start">
+                            {
+                                historico.map((li,index) => (
+                                    <li key={index}>
+                                        {li.status} - {li.valor}
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    ): (
+                        <p>Nada feito ainda</p>
+                    )
+                }
+            </div>
             <span>
                 {contador}
             </span>
             <div className="flex gap-1 *:bg-sky-500 *:p-2">
-                <button>
+                <button onClick={incrementar}>
                     incrementar
                 </button>
-                <button>
+                <button onClick={decrementar}>
                     decrementar
                 </button>
             </div>
-            <button className="p-2 bg-emerald-500">
+            <button className="p-2 bg-emerald-500" onClick={zerar}>
                 zerar
             </button>
         </div>
