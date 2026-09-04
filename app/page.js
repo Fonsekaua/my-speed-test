4f8d929b212abb2cba9fd5c0a1fc31cd46dfa2a9
fonsekaua <fonsecakauasilva3@gmail.com>
2026-09-04 15:27:18 -0500

    criado input para buscar cep

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import TrocarTema from "@/src/views/teste01";
 import GaleriaImagens from "@/src/views/teste02";
 import ContadorHistorico from "@/src/views/teste03";
+import BuscarCEP from "@/src/views/teste04";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <ContadorHistorico />
+    <BuscarCEP />
   );
 }
--- src/views/teste04.jsx
+++ src/views/teste04.jsx
@@ -1,7 +1,16 @@
-const BuscarCEP = () => {
-    return (
-        <div className="bg-zinc-900 text-white">
+'use client'
+import { useState } from "react"
 
+const BuscarCEP = () => {
+    const [input,setInput] = useState("")
+    return (
+        <div className="w-screen h-screen bg-zinc-900 text-white">
+           <div>
+             <input type="text" placeholder="Digite seu cep" />
+            <button>
+                Buscar
+            </button>
+           </div>
         </div>
     )
 }
