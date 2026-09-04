1b25d09580a9a4394c31d91ebbf2dbf651576a1b
fonsekaua <fonsecakauasilva3@gmail.com>
2026-09-04 14:54:56 -0500

    imagens sendo listadas corretamente

--- src/views/teste02.jsx
+++ src/views/teste02.jsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
+'use client'
+import { useEffect, useState } from "react";
 
 const GaleriaImagens = () => {
     const [imagens,setImagens] = useState([])
-    return (
-        <div className="bg-zinc-800 w-screen h-screen">
+    useEffect(() => {
+        const inserirImagens = () => {
+            let list = []
+            for(let i = 1; i <= 6;i++) {
+                list.push(`img0${i}.jpg`)
+            }
 
+            setImagens(list)
+        }
+        inserirImagens()
+    },[])
+    return (
+        <div className="bg-zinc-800 w-screen h-screen flex items-center justify-center p-10 flex-wrap">
+            {
+                imagens.map((img,index) => (
+                    <div key={index}>
+                        <img  className="w-56 h-56" src={`/${img}`} alt="" />
+                    </div>
+                ))
+            }
         </div>
     )
 }
